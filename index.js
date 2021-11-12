cebba0ca8124db4df217613bbc6e26c8448bcec0
Michael Joyce <michael.joyce@illiniarts.com>
2021-11-12 13:57:49 -0600

    Add tray icon
    No functionality yet

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const electron = require('electron');
+const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
+    // main window
     mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false
-        }
+            contextIsolation: false,
+        },
+        height: 500,
+        width: 300,
+        frame: false,
+        resizable: false
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+    // tray icon
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+    new Tray(iconPath);
 });
