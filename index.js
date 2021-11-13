bdb323cd8eadc5e87bde47313d69f6674c3387f8
Michael Joyce <michael.joyce@illiniarts.com>
2021-11-13 15:11:08 -0600

    Show and hide window with icon in tray

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const electron = require('electron');
 const path = require('path');
+const { cli } = require('webpack');
 
 const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
+
     // main window
     mainWindow = new BrowserWindow({
         webPreferences: {
@@ -15,11 +18,24 @@ app.on('ready', () => {
         height: 500,
         width: 300,
         frame: false,
-        resizable: false
+        resizable: false,
+        show: false
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
     // tray icon
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-    new Tray(iconPath);
+    tray = new Tray(iconPath);
+
+    // show icon on icon click
+    tray.on('click', () => {
+        if (mainWindow.isVisible()) {
+            mainWindow.hide();
+        }
+        else {
+            mainWindow.show();
+        }
+    });
+
 });
