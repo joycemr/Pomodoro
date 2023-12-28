6dc1a39a33988725b95b3790f9f185c370ede701
Michael Joyce <michael.joyce@illiniarts.com>
2023-12-28 14:13:11 -0600

    Add context menu

--- app/timer_tray.js
+++ app/timer_tray.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const { Tray } = electron;
+const { app, Tray, Menu } = electron;
 
 class TimerTray extends Tray {
 
@@ -8,6 +8,7 @@ class TimerTray extends Tray {
         this.mainWindow = mainWindow;
         this.setToolTip('Timer App');
         this.on('click', this.onClick.bind(this));
+        this.on('right-click', this.onRightClick.bind(this));
     }
 
     onClick = (event, bounds) => {
@@ -26,6 +27,16 @@ class TimerTray extends Tray {
                 width
             });
         }
+    }
+
+    onRightClick = () => {
+        const menuConfig = Menu.buildFromTemplate([
+            {
+                label: 'Quit',
+                click: () => app.quit()
+            }
+        ]);
+        this.popUpContextMenu(menuConfig);
     }
 
 };
